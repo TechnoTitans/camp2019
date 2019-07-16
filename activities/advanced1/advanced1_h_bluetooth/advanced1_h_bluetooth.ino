73d1941686373acfade732e9733ea413bc0e43b4
Berk Ott <berkott3002@gmail.com>
2019-07-16 00:31:32 -0400

    Update advanced1_h_bluetooth.ino

--- activities/advanced1/advanced1_h_bluetooth/advanced1_h_bluetooth.ino
+++ activities/advanced1/advanced1_h_bluetooth/advanced1_h_bluetooth.ino
@@ -1,15 +1,15 @@
 #include <SoftwareSerial.h> // this allows us to use the bluetooth module
 
-const int TX_PIN = 10;
-const int RX_PIN = 9;
-const int ledPin = LED_BUILTIN; // blinks the led on the actual arduino
+int TX_PIN = 10;
+int RX_PIN = 9;
+int ledPin = LED_BUILTIN; // blinks the led on the actual arduino
 
-SoftwareSerial bluetoothSerial(TX_PIN, RX_PIN); // TX/RX pins
+SoftwareSerial bluetoothSerial(TX_PIN, RX_PIN); // TX/RX pins don't worry about this part
 
 void setup()
 {
 	bluetoothSerial.begin(115200); // similar to our regular serial console
-	Serial.begin(9600);
+	/* [blank] */; // [blank] how do you start serial console?
 
 	bluetoothSerial.println("==== Bluetooth Serial ====");
 	Serial.println("==== Regular Serial ====");
@@ -21,10 +21,10 @@ void loop()
 {
   // char is a character
 	char bluetoothData = bluetoothSerial.read();		// [blank] since the bluetooth acts as a serial, how do we read a string?
-	if(bluetoothData == '1') {
-    digitalWrite(ledPin, HIGH);  
-	} else if (bluetoothData == '2') {
-    digitalWrite(ledPin, LOW);
+	/* [blank] */ // [blank] how do we see if the Bluetooth data is 1
+    digitalWrite(/* [blank] */); // [blank] how do we turn on the led?
+	} /* [blank] */ if (bluetoothData == '2') { // [blank] how do we complete the if statement?
+    digitalWrite(/* [blank] */); // [blank] how do we turn off the led?
 	}
 //	Serial.print("Bluetooth Serial Data: ");	// hint: look back at intermediate4
 //  Serial.println(bluetoothData);
