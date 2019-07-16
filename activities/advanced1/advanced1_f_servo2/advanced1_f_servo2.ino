00849e7aa192f67abea0c930765399d24ac329e5
Berk Ott <berkott3002@gmail.com>
2019-07-16 00:24:34 -0400

    Update advanced1_f_servo2.ino

--- activities/advanced1/advanced1_f_servo2/advanced1_f_servo2.ino
+++ activities/advanced1/advanced1_f_servo2/advanced1_f_servo2.ino
@@ -1,9 +1,9 @@
 #include <Servo.h>
 
 
-const int servoPin = 11; // PWM pin for the servo
-const int maxAngle = 90; // the servo can only rotate up to +90 degrees
-const int minAngle = -90; // or -90 degrees
+int servoPin = /* [blank] */; // blank pin for servo signal PWM pin for the servo
+int maxAngle = 90; // the servo can only rotate up to +90 degrees
+int minAngle = -90; // or -90 degrees
 
 int angle = 0;	// this variable will keep track of the current angle of our servo
 					// in this case we are going to start off at -90
@@ -12,12 +12,12 @@ Servo servo;
 
 void setup()
 {
-  servo.attach(servoPin); // tell the servo object to use the servo on pin 11
+  servo.attach(/* [blank] */); // blank what pin do we attatch? tell the servo object to use the servo from pin variable
 }
 
 void loop()
 {
-	angle = angle + 5; // [blank] what do we do to the angle so that it changes over time?
+	angle /* [blank] */; // [blank] what do we do to the angle so that it changes over time?
 	servo.write(angle);  
   delay(100);
 }
